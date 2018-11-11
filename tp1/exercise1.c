5dfaafae4266f3ccfbc5b81bd92a33912787c521
Jules Lamur <contact@juleslamur.fr>
2018-11-11 21:37:02 +0100

    [tp1] stop bubble sort when k elements are found.

--- tp1/exercise1.c
+++ tp1/exercise1.c
@@ -2,16 +2,18 @@
 void sort_and_get_k_first(unsigned int n, int *numbers, unsigned int k, int *result)
 {
     for (unsigned int i = 0; i < n; i++) {
-        for (unsigned int j = 0; j < n; j++) {
-            if (numbers[i] > numbers[j]) {
+        for (unsigned int j = i; j < n; j++) {
+            if (numbers[i] < numbers[j]) {
                 int t = numbers[i];
                 numbers[i] = numbers[j];
                 numbers[j] = t;
             }
         }
-    }
 
-    for (unsigned int i = 0; i < k; i++) {
         result[i] = numbers[i];
+
+        if (i > k) {
+            break;
+        }
     }
 }
