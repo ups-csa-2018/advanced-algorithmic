589ab0ca77941ced86ef508126cd3d55fc778b0d
Jules Lamur <contact@juleslamur.fr>
2018-11-13 19:27:48 +0100

    [tp1] oops, exercise1 was selection not bubble sort.

--- tp1/exercise1.c
+++ tp1/exercise1.c
@@ -1,19 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+
 // bubble sort
 void sort_and_get_k_first(unsigned int n, int *numbers, unsigned int k, int *result)
 {
-    for (unsigned int i = 0; i < n; i++) {
-        for (unsigned int j = i; j < n; j++) {
-            if (numbers[i] < numbers[j]) {
-                int t = numbers[i];
-                numbers[i] = numbers[j];
-                numbers[j] = t;
+    int *sorted_numbers = malloc(sizeof(int) * n);
+    memcpy(sorted_numbers, numbers, sizeof(int) * n);
+
+    for (unsigned int i = 0; i < k; i++) {
+        for (unsigned int j = 0; j + 1 < n - i; j++) {
+            if (sorted_numbers[j] > sorted_numbers[j + 1]) {
+                int t = sorted_numbers[j];
+                sorted_numbers[j] = sorted_numbers[j + 1];
+                sorted_numbers[j + 1] = t;
             }
         }
 
-        result[i] = numbers[i];
+        result[i] = sorted_numbers[n - 1 - i];
 
-        if (i > k) {
+        if (i == k) {
             break;
         }
     }
+
+    free(sorted_numbers);
 }
